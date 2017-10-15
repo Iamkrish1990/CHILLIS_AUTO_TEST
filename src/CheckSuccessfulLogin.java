24f475319908c9a9f3530c2d4a1324d90a0700af
Iamkrish1990 <krishnendu1990@gmail.com>
2017-10-15 20:29:12 +0530

    packages written manually.

--- src/CheckSuccessfulLogin.java
+++ src/CheckSuccessfulLogin.java
@@ -1,4 +1,11 @@
 import org.openqa.selenium.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import java.util.concurrent.TimeUnit;
+
 
 public class CheckSuccessfulLogin {
 
