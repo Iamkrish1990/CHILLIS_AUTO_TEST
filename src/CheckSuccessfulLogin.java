16e5cd3497e2fb53f2bcef417f9b6f48fb304742
Iamkrish1990 <krishnendu1990@gmail.com>
2017-10-15 21:24:39 +0530

    tstng output file added

--- src/CheckSuccessfulLogin.java
+++ src/CheckSuccessfulLogin.java
@@ -5,8 +5,25 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.*;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
 
 
-public class CheckSuccessfulLogin {
-
+public class CheckSuccessfulLogin{
+	WebDriver driver;
+	@BeforeMethod
+	 public void beforeMethod(){
+	
+	System.setProperty("WebDriver.Chrome.ChromeDriver","C:\\Users\\Asus PC\\Desktop\\Eclipse Neon and Selenium");
+	driver=new ChromeDriver();
+	}
+	@Test
+	public void login() throws InterruptedException
+	{
+		driver.navigate().to("https://www.chilis.com/");
+	}
+	
 }
